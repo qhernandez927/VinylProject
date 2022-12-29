0d89f3ea029f8c4b3225865e5f952e53b5a5a3bd
Quinn Hernandez <quinnh2791@gmail.com>
2022-12-29 12:39:44 -0700

    light changes

--- Home.js
+++ Home.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
-
-import {TailwindProvider} from 'tailwind-rn';
-import utilities from './tailwind.json';
+import {Text, View} from 'react-native';
 import {useTailwind} from 'tailwind-rn';
 
 const Home = () => {
--- README.md
+++ README.md
@@ -4,5 +4,6 @@ App to track my lifting data
 
 npx react-native run-ios
 
+https://github.com/vadimdemedes/tailwind-rn/issues/156
 yarn build:tailwind for styling
 yarn dev:tailwind watch for style changes
