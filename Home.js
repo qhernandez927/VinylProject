0153412243381d578c0056439ad7c481379aea6d
Quinn Hernandez <quinnh2791@gmail.com>
2022-12-29 22:28:43 -0700

    added temp log out button

--- Home.js
+++ Home.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {TouchableOpacity, Text, View} from 'react-native';
 import {useTailwind} from 'tailwind-rn';
+import auth from '@react-native-firebase/auth';
 
 const Home = () => {
   const tw = useTailwind();
 
+  const logOff = () => {
+    // TODO: check if user is logged in
+    auth()
+      .signOut()
+      .then(() => console.log('Youre Signed out'));
+  };
+
   return (
     <View style={tw('pt-11 items-center')}>
+      <TouchableOpacity onPress={logOff}>
+        <Text>Temp Log Out</Text>
+      </TouchableOpacity>
       <View style={tw('bg-blue-200 px-3 py-1 rounded-full')}>
         <Text style={tw('text-blue-800 font-semibold')}>Hello Tailwind</Text>
       </View>
